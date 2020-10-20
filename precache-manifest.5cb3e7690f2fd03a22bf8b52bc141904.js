f2a222e171e54dc4f9449b7cda64300dd02e8d0f
bianjun0810 <bianjun0810@gmail.com>
2020-10-21 00:26:49 +0900

    Updates

---
+++ precache-manifest.5cb3e7690f2fd03a22bf8b52bc141904.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "008ba390996dd1b0ba16dfb4d425826d",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "60a2c7e5388dd1c73a67",
+    "url": "/movie_app/static/css/main.b9bd5515.chunk.css"
+  },
+  {
+    "revision": "2cb00f0b337cb1b1485d",
+    "url": "/movie_app/static/js/2.03729dac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.03729dac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60a2c7e5388dd1c73a67",
+    "url": "/movie_app/static/js/main.9667f99f.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
